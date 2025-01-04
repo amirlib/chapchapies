52c672088df231143c8f6ce6fe77ca21aecb8e1d
amir.liberzon <amir@rezonate.io>
2025-01-04 11:44:12 +0200

    configurations

--- eslint.config.cjs
+++ eslint.config.cjs
@@ -1,40 +1,41 @@
 const nx = require('@nx/eslint-plugin');
+import stylistic from '@stylistic/eslint-plugin-ts';
 
 module.exports = [
-  ...nx.configs['flat/base'],
-  ...nx.configs['flat/typescript'],
-  ...nx.configs['flat/javascript'],
-  {
-    ignores: ['**/dist'],
-  },
-  {
-    files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx'],
-    rules: {
-      '@nx/enforce-module-boundaries': [
-        'error',
-        {
-          enforceBuildableLibDependency: true,
-          allow: ['^.*/eslint(\\.base)?\\.config\\.[cm]?js$'],
-          depConstraints: [
-            {
-              sourceTag: '*',
-              onlyDependOnLibsWithTags: ['*'],
-            },
-          ],
-        },
-      ],
-    },
-  },
-  {
-    files: [
-      '**/*.ts',
-      '**/*.tsx',
-      '**/*.js',
-      '**/*.jsx',
-      '**/*.cjs',
-      '**/*.mjs',
-    ],
-    // Override or add rules here
-    rules: {},
-  },
+	...nx.configs['flat/base'],
+	...nx.configs['flat/typescript'],
+	...nx.configs['flat/javascript'],
+	{
+		ignores: [
+			'**/*.generated.ts',
+			'**/.github',
+			'**/dist',
+			'**/generated',
+			'**/node_modules',
+		],
+	},
+	stylistic.configs['all-flat'],
+	{
+		files: ['**/*.ts', '**/*.js'],
+		rules: {
+			'@nx/enforce-module-boundaries': [
+				'error',
+				{
+					enforceBuildableLibDependency: true,
+					allow: ['^.*/eslint(\\.base)?\\.config\\.[cm]?js$'],
+					depConstraints: [
+						{
+							sourceTag: '*',
+							onlyDependOnLibsWithTags: ['*'],
+						},
+					],
+				},
+			],
+		},
+	},
+	{
+		files: ['**/*.ts', '**/*.js'],
+		// Override or add rules here
+		rules: {},
+	},
 ];
